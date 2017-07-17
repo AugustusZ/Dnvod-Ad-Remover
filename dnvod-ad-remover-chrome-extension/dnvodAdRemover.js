65495b0a0b96711bf902cfe6643b3c92d3232366
AugustusZ <augustus.z@outlook.com>
2017-07-17 03:50:48 -0700

    Refactored code

--- dnvod-ad-remover-chrome-extension/dnvodAdRemover.js
+++ dnvod-ad-remover-chrome-extension/dnvodAdRemover.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMNodeInserted', function (event) {
+	autoplay();
+	removeAds();
+});
+
+autoplay = () => {
 	// auto play
 	var videoPlayer = document.getElementsByTagName('video')[0];
 	if (videoPlayer) {
@@ -7,79 +12,38 @@ document.addEventListener('DOMNodeInserted', function (event) {
 			videoPlayer.play();
 		});
 	}
+}
 
-	// remove video ad
-	var parentNode = document.getElementsByClassName("HTML5-only")[0];
-	var childNode = document.getElementsByClassName("ads-control")[0];
-	if (parentNode && childNode) {
-		console.log('Removing ad');
-		parentNode.removeChild(childNode);
+removeNode = (node) => {
+	if (node) {
+		node.parentElement.removeChild(node);
 	}
+}
 
-	// remove play page right side next-to-video ad
-	var parentNode = document.getElementsByClassName("bfq")[0];
-	var childNode = document.getElementsByClassName("bfq-r")[0];
-	if (parentNode && childNode) {
-		console.log('Removing ad');
-		parentNode.removeChild(childNode);
+removeById = (id) => {
+	var node = document.getElementById(id);
+	removeNode(node);
+}
+
+removeClassFromClass = (className) => {
+	var childNodes = document.getElementsByClassName(className);
+	for (var j = childNodes.length - 1; j >= 0; j--) {
+		removeNode(childNodes[j]);
 	}
+}
 
+removeAds = () => {
+	removeClassFromClass('ads-control'); // remove video-covering ad
+	removeClassFromClass('bfq-r'); // remove play page right side next-to-video ad
 	removeById('myaudient2'); // remove homepage header banner ad
-
-	// remove list page banner ad
-	var parentNode = document.getElementsByClassName("product")[0];
-	var childNodes = document.getElementsByClassName("ggw-l");
-	if (parentNode && childNodes) {
-		console.log('Removing ad');
-		for (var i = childNodes.length - 1; i >= 0; i--) {
-			parentNode.removeChild(childNodes[i]);
-		}
-	}
-
-	// remove detail page banner ad
-	var parentNode = document.getElementsByClassName("left")[0];
-	var childNodes = document.getElementsByClassName("ggw-l");
-	if (parentNode && childNodes) {
-		console.log('Removing ad');
-		for (var i = childNodes.length - 1; i >= 0; i--) {
-			parentNode.removeChild(childNodes[i]);
-		}
-	}
-
-	// remove homepage next-to-logo ad
-	var parentNode = document.getElementsByClassName("top")[0];
-	var childNodes = document.getElementsByClassName("tgg");
-	if (parentNode && childNodes) {
-		console.log('Removing ad');
-		for (var i = childNodes.length - 1; i >= 0; i--) {
-			parentNode.removeChild(childNodes[i]);
-		}
-	}
-
-	// remove right squre ad
-	var parentNode = document.getElementsByClassName("right")[0];
-	var childNodes = document.getElementsByClassName("r-gg");
-	if (parentNode && childNodes) {
-		console.log('Removing ad');
-		for (var i = childNodes.length - 1; i >= 0; i--) {
-			parentNode.removeChild(childNodes[i]);
-		}
-	}
-
-	removeById('msg_winw'); // remove corner ad
-
-	// remove body banner ad
-	var parentNode = document.getElementsByClassName("main")[0];
-	var childNodes = document.getElementsByClassName("ggw");
-	if (parentNode && childNodes) {
-		console.log('Removing ad');
-		for (var i = childNodes.length - 1; i >= 0; i--) {
-			parentNode.removeChild(childNodes[i]);
-		}
-	}
+	removeClassFromClass('ggw-l'); // remove list page banner ad
+	removeClassFromClass('ggw-l'); // remove detail page banner ad
+	removeClassFromClass('tgg'); // remove homepage next-to-logo ad
+	removeClassFromClass('r-gg'); 	// remove right squre ad
+	removeById('msg_winw'); // remove corner window ad
+	removeClassFromClass('ggw'); // remove body banner ad
 
 	// remove homepage list tail ad
-	// document.getElementsByClassName('i-cp')[0].getElementsByTagName('ul')[0].getElementsByClassName('gg')
 	var parentNodes = document.getElementsByClassName('i-cp');
 	for (var i = parentNodes.length - 1; i >= 0; i--) {
 		var childNodes = parentNodes[i].getElementsByTagName('ul');
@@ -90,12 +54,6 @@ document.addEventListener('DOMNodeInserted', function (event) {
 			}
 		}
 	}
-});
-
-removeById = (id) => {
-	var childNode = document.getElementById(id);
-	if (childNode) {
-		childNode.parentElement.removeChild(childNode);
-	}
 }
+
 
