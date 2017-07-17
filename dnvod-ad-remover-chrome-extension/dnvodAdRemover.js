c27793dc5b2aff84759a12cfd70133f595c1407c
AugustusZ <augustus.z@outlook.com>
2017-07-17 04:02:22 -0700

    Refactored code

--- dnvod-ad-remover-chrome-extension/dnvodAdRemover.js
+++ dnvod-ad-remover-chrome-extension/dnvodAdRemover.js
@@ -1,10 +1,9 @@
-document.addEventListener('DOMNodeInserted', function (event) {
+document.addEventListener('DOMNodeInserted', (event) => {
 	autoplay();
 	removeAds();
 });
 
 autoplay = () => {
-	// auto play
 	var videoPlayer = document.getElementsByTagName('video')[0];
 	if (videoPlayer) {
 		videoPlayer.oncanplay = (_ => {
@@ -26,34 +25,23 @@ removeById = (id) => {
 }
 
 removeClassFromClass = (className) => {
-	var childNodes = document.getElementsByClassName(className);
-	for (var j = childNodes.length - 1; j >= 0; j--) {
-		removeNode(childNodes[j]);
+	var nodes = document.getElementsByClassName(className);
+	for (var i = nodes.length - 1; i >= 0; i--) {
+		removeNode(nodes[i]);
 	}
 }
 
 removeAds = () => {
-	removeClassFromClass('ads-control'); // remove video-covering ad
-	removeClassFromClass('bfq-r'); // remove play page right side next-to-video ad
 	removeById('myaudient2'); // remove homepage header banner ad
+	removeById('msg_winw'); // remove all pages' corner window ad
+	removeClassFromClass('tgg'); // remove homepage next-to-logo ad
+	removeClassFromClass('r-gg'); // remove right squre ad
+	removeClassFromClass('ggw'); // remove body banner ad
 	removeClassFromClass('ggw-l'); // remove list page banner ad
 	removeClassFromClass('ggw-l'); // remove detail page banner ad
-	removeClassFromClass('tgg'); // remove homepage next-to-logo ad
-	removeClassFromClass('r-gg'); 	// remove right squre ad
-	removeById('msg_winw'); // remove corner window ad
-	removeClassFromClass('ggw'); // remove body banner ad
-
-	// remove homepage list tail ad
-	var parentNodes = document.getElementsByClassName('i-cp');
-	for (var i = parentNodes.length - 1; i >= 0; i--) {
-		var childNodes = parentNodes[i].getElementsByTagName('ul');
-		for (var j = childNodes.length - 1; j >= 0; j--) {
-			var grandchildNode = childNodes[j].getElementsByClassName('gg')[0];
-			if (grandchildNode) {
-				childNodes[j].removeChild(grandchildNode);
-			}
-		}
-	}
+	removeClassFromClass('bfq-r'); // remove play page next-to-video ad
+	removeClassFromClass('ads-control'); // remove play page video-covering ad
+	removeClassFromClass('gg'); // remove homepage list tail ad
 }
 
 
